36ad7ccd8b2747ca6afad64932a8fbcaadd5b7c0
Thiago Valentim <thiago@criawebs.com.br>
2019-12-22 18:10:04 -0300

    Added Legend component that is responsible for the exhibition of the lane names from the Kanban.
    chore: implement the rickshaw legend component to show the lane names and creating the respectives behaviors or animations.
    chore: add the behavior and child components (Highlight and Toggle)

---
+++ src/app/components/CFD/Graph/Legend/Legend.tsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import Toggle from "./Behavior/Toggle";
+import Highlight from "./Behavior/Highlight";
+const Graph = require('rickshaw').Graph;
+
+interface LegendProps {
+    graph: any;
+}
+
+export default class Legend extends Component<LegendProps> {
+    componentDidMount() {
+        const legend = new Graph.Legend( {
+            graph: this.props.graph,
+            element: document.getElementById('legend')
+        } );
+
+        new Toggle({graph: this.props.graph, legend});
+        new Highlight({graph: this.props.graph, legend});
+    }
+
+    render() {
+        return (
+            <div id="legend"></div>
+        );
+    }
+}
