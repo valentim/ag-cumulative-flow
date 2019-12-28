d53ac15401e644635bfae0d6abe4eb230aacb52b
Thiago Valentim <thiago@criawebs.com.br>
2019-12-28 12:41:36 -0300

    refactory: include the div with the cumulative id inside the component

--- src/app/components/CFD/CFD.tsx
+++ src/app/components/CFD/CFD.tsx
@@ -8,7 +8,10 @@ interface CumulativeFlowDataProps {
 export default class CFD extends Component<CumulativeFlowDataProps> {
     render() {
         return (
-            <CFDContent data={this.props.data} />
+            <div>
+                <div id="cumulative"></div>
+                <CFDContent data={this.props.data} />
+            </div>
         );
     }
 }
